1f711b056d844f71fb20d864d18d984f1a880948
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2023-02-15 14:20:52 -0300

    implements jwt authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
+         :timeoutable,
          jwt_revocation_strategy: JwtDenylist
 end
